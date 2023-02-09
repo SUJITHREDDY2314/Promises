15d1d223ceb33c2648f311197aaf696956893f7f
SUJITHREDDY2314 <mereddysujithreddy@gmail.com>
2023-02-09 15:10:13 +0530

    Added changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import Child from "./Child";
-import React from "react";
+import React, { useState } from "react";
 function App() {
-  const [dataToPass, setData] = React.useState({});
-
-
+  const [dataToPass, setData] = useState({});
+  const [isClicked, setIsClicked] = useState(false);
   React.useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos/1")
       .then((response) => response.json())
       .then((res) => setData(res));
-  }); 
+  }, [isClicked]);
   // to avoid setting every time and infinite loop
-
+  const [changed, setChanged] = useState(false);
   const funcToReset = () => {
-    console.log("iam invoked");
-    setData({});
+    if (changed) {
+      setIsClicked((prev) => !prev);
+    } else {
+      console.log("iam invoked");
+      setData({});
+    }
+    setChanged((prev) => !prev);
   };
 
   return (
     <div className="App">
       <p>My react app</p>
-      <Child key={dataToPass.id} test="hi" obj={dataToPass} fun={funcToReset} />
+      <Child
+        key={dataToPass.id}
+        test="hi"
+        obj={dataToPass}
+        fun={funcToReset}
+        click={changed}
+      />
     </div>
   );
 }
